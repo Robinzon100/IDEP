15bc8b0b7fd71bcf11a9a6270c769ca46193d8b7
Robinzon100 <robinzon.sokhashvili@gmail.com>
2021-03-10 06:21:46 +0400

    :rocket: finished the video section and 3rd idep swap

--- components/pages/landing/hero/Hero.tsx
+++ components/pages/landing/hero/Hero.tsx
@@ -5,14 +5,17 @@ import hammerjs from "hammerjs";
 import { planetAnimation, fadeOutFadeIn, dissableBlurOnElement } from './utils/animations/hero.animations';
 import Button from 'components/lib/button/Button';
 import { fixScrollToTop } from 'components/utils/animations/gsap.animations';
+import { FC } from 'react';
+import gsap from "gsap";
+import { ScrollTrigger } from "gsap/dist/ScrollTrigger";
+gsap.registerPlugin(ScrollTrigger)
 
 
 
 
 
 
-
-const Hero: React.FC = () => {
+const Hero: FC = () => {
 
     useEffect(() => {
         console.log(navigator.userAgent.search('Firefox') > -1)
@@ -102,17 +105,48 @@ const Hero: React.FC = () => {
         // videoIntro.addEventListener('ended', ({ currentTarget }) => {
         fadeOutFadeIn('.intro_video', ['.hero_canvas', '.title'])
 
+        let tl = gsap.timeline({
+            scrollTrigger: {
+                trigger: '.hero_container',
+                start: 'top top',
+                scrub: 1,
+                pin: true
+            }
+        })
 
 
-        const heroCanvasGestures = new hammerjs(canvas as HTMLElement)
-        heroCanvasGestures.get('pan').set({ direction: Hammer.DIRECTION_VERTICAL });
-        heroCanvasGestures.on('panup pandown pan swipe', (e: any) => {
-            planetAnimation(e, camera, e.type)
+        tl.to(camera.position, {
+            duration: 3,
+            z: 4,
+            y: 0.9,
+            ease: "Expo.easeOut"
         })
-        canvas.addEventListener('mousewheel', (e: WheelEvent) => {
-            // console.log(e)
-            planetAnimation(e, camera)
+        tl.to('.title', {
+            duration: 1,
+            y: 0,
+            opacity: 1,
+            ease: "Expo.easeOut",
         })
+        tl.to('.title', {
+            y: 80,
+            opacity: 0,
+            ease: "Expo.easeOut",
+        })
+        tl.to(['.main_header', '.hero_button', '.navigation'], {
+            display: 'initial',
+            duration: 4,
+            opacity: 1,
+            y: 0
+        })
+        // const heroCanvasGestures = new hammerjs(canvas as HTMLElement)
+        // heroCanvasGestures.get('pan').set({ direction: Hammer.DIRECTION_VERTICAL });
+        // heroCanvasGestures.on('panup pandown pan swipe', (e: any) => {
+        //     planetAnimation(e, camera, e.type)
+        // })
+        // canvas.addEventListener('mousewheel', (e: WheelEvent) => {
+        //     // console.log(e)
+        //     planetAnimation(e, camera)
+        // })
         // })
 
 
@@ -141,7 +175,7 @@ const Hero: React.FC = () => {
         scene.add(ring)
         renderer.setClearColor(0xffffff, 0)
         canvas.appendChild(renderer.domElement);
-        fixScrollToTop(1) 
+        fixScrollToTop(1)
         animate()
     }
 
