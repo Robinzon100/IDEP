4b3f3ef954c261454dd27d42180b864061591e85
Robinzon100 <robinzon.sokhashvili@gmail.com>
2021-03-12 09:48:10 +0400

    :rocket: added metta tags and image

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,4 +1,5 @@
 import type { AppProps } from 'next/app'
+import Head from "next/head"
 import '../styles/main.scss'
 // import Head from 'next/head';
 
@@ -6,6 +7,29 @@ import '../styles/main.scss'
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
+      <Head>
+        <title>IDEP</title>
+        <link rel="shortcut icon" href="/logo_mark.svg" />
+      </Head>
+      <Head>
+        <meta name="title" content="🌌IDEP" />
+        <meta name="description" content="IDEP will introduce standards and tools that will enable a new economy based on blockchain powered data and digital media."
+        />
+        <meta property="og:type" content="website" />
+        <meta property="og:url" content="https://idep-plum.vercel.app/og_image.png"
+        />
+        <meta property="og:title" content="🌌IDEP" />
+        <meta property="og:description" content="IDEP will introduce standards and tools that will enable a new economy based on blockchain powered data and digital media."
+        />
+        <meta property="og:image" content="/og_image.png" />/
+        <meta property="twitter:card" content="summary_large_image" />
+        <meta property="twitter:url" content="https://idep-plum.vercel.app/og_image.png"
+        />
+        <meta property="twitter:title" content="🌌IDEP" />
+        <meta property="twitter:description" content="IDEP will introduce standards and tools that will enable a new economy based on blockchain powered data and digital media."
+        />
+        <meta property="twitter:image" content="/og_image.png" />
+      </Head>
       <Component {...pageProps} />
     </>
   )
